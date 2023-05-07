2b921881d8db9ba7f16eb8c583a43078f32d637a
masudcse92 <masudcse9292@gmail.com>
2023-05-08 01:18:19 +0600

    firebase install

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb6ZcQ77GiPrLO-r7iw-o2UTHxPmeqfzI",
+  authDomain: "ema-john-with-router-aut-fe047.firebaseapp.com",
+  projectId: "ema-john-with-router-aut-fe047",
+  storageBucket: "ema-john-with-router-aut-fe047.appspot.com",
+  messagingSenderId: "655085628772",
+  appId: "1:655085628772:web:ff7dd2d6045511b9e21e71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
